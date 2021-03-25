0b613e059d079262b37011608443c4df87a4d86e
akhil091 <akhilchandail9@gmail.com>
2021-03-25 15:07:13 +0530

    first commit

---
+++ Lab/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from Lab.models import NotSure,Tests,HealthPackage,Condition,Order,Cart,Address, ZipCode
+
+@admin.register(NotSure)
+class NotSureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tests)
+class TestsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(HealthPackage)
+class HealthPackageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Condition)
+class ConditionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    pass    
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ZipCode)
+class ZipCodeAdmin(admin.ModelAdmin):
+    pass
